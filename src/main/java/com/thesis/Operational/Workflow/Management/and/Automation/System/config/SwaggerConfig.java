fdeada876219ce0c0b1fd3b5d5380b911666c89f
OrdeInn <emiroguz98@gmail.com>
2022-03-05 21:10:26 +0300

    swagger added

--- src/main/java/com/thesis/Operational/Workflow/Management/and/Automation/System/config/SwaggerConfig.java
+++ src/main/java/com/thesis/Operational/Workflow/Management/and/Automation/System/config/SwaggerConfig.java
@@ -22,33 +22,8 @@ import java.util.Collections;
 import java.util.List;
 
 @Configuration
-@Import({ BeanValidatorPluginsConfiguration.class, SpringDataRestConfiguration.class })
 @EnableSwagger2
 public class SwaggerConfig{
-
-    @NotNull
-    private List<SecurityScheme> securitySchema() {
-        return Collections.singletonList(
-                OAuth2Scheme.OAUTH2_PASSWORD_FLOW_BUILDER
-                        .name("Credentials")
-                        .tokenUrl("/api/auth/login-swagger-method-not-seen-endpoint")
-                        .scopes(Collections.singletonList(new AuthorizationScope("Default", "Default scope.")))
-                        .build()
-        );
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext
-                .builder()
-                .operationSelector(
-                        o -> PathSelectors
-                                .ant("/api/auth/*")
-                                .negate()
-                                .test(o.requestMappingPattern())
-                )
-                .securityReferences(Collections.singletonList(new SecurityReference("Credentials", new AuthorizationScope[0])))
-                .build();
-    }
 
     /**
      * Creates the Swagger configuration bean.
@@ -58,38 +33,23 @@ public class SwaggerConfig{
     @Bean
     public Docket api() {
 
-        return new Docket(DocumentationType.OAS_30).groupName("DELTA").select()
-                .apis(RequestHandlerSelectors.basePackage("com.thesis.Operational.Workflow.Management.and.Automation.System.controllers"))
-                .paths(PathSelectors.any()).build()
-                .securityContexts(Collections.singletonList(securityContext()))
-                .securitySchemes(securitySchema())
-                .apiInfo(apiInfo("Final Project API",
-                        "This api adheres to the RESTfull properties and all entities can be accessed with pagination and sorting. Full table is not sent, only pages. "
-                                + "For more info, search pagination on google.\n" + "\n"
-                                + "## There are 2 kinds of items in this document, Entity and Controller. Entity items document the direct access to an entity for CRUD operations, while controllers provide custom endpoints.\n"
-                                + "\n" + "## EDITING AN ENTRY\n"
-                                + "For editing, use PATCH commands and give only the thin you edit. Put replaces the whole object, while patch only updates it with the given attributes.\n"
-                                + "\n" + "## REFERENCING OBJECTS\n"
-                                + "For example, while assigning a company to user, send a POST request to the /user/id/company address with the content type \"text/uri-list\" and body \"../api/companies/id\"."));
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.thesis.Operational.Workflow.Management.and.Automation.System"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(metaData());
     }
 
-    @Bean
-    public UiConfiguration uiConfig() {
-
-        return UiConfigurationBuilder.builder().operationsSorter(OperationsSorter.METHOD).build();
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Final Project Spring Boot Swagger Configuration")
+                .description("\"Swagger configuration for application \"")
+                .version("1.1.0")
+                .license("Apache 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Tech Interface", "https://www.youtube.com/channel/UCMpJ8m1w9t7EFcF9x8rs02A", "info@techinterface.com"))
+                .build();
     }
-
-    /**
-     * Creates an object containing API information including author name,
-     * email, version, license, etc.
-     *
-     * @param title API title
-     * @param description API description
-     * @return API information
-     */
-    private ApiInfo apiInfo(String title, String description) {
-
-        return new ApiInfoBuilder().title(title).description(description).version("1.0.0-Snapshot").build();
-    }
-
+    //for Swagger api doc generation
+    //http://localhost:8080/v2/api-docs
 }
